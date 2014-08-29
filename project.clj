bb9841740177ee9c50a1b4a09d1b6356cce23e9e
Toby Nance <toby.nance@gmail.com>
2014-08-29 13:46:53 -0500

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject cards "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.tobynance/cards "0.1.0-SNAPSHOT"
+  :description "Card game library"
+  :url "https://github.com/tobynance/cards"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
